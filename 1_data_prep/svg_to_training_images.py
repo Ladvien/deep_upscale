c590f9f04f7e332bbc39748aa48b8829bd97c1ed
ladvien <honeysucklehardware@gmail.com>
2020-12-01 06:54:16 -0600

    Adding datasets

--- 1_data_prep/svg_to_training_images.py
+++ 1_data_prep/svg_to_training_images.py
@@ -14,12 +14,6 @@ git clone https://github.com/encharm/Font-Awesome-SVG-PNG
 ################
 # Pre Execution
 ################
-# Get data
-# !git clone https://github.com/encharm/Font-Awesome-SVG-PNG.git
-
-# Install CairoSVG
-# Installation instructions.
-# !pip install cairosvg
 
 
 ###########
@@ -39,12 +33,13 @@ import matplotlib.pyplot as plt
 # Setup
 #############
 root_path = os.environ["HOME"]
+project_path = f"{root_path}/deep_upscale/"
 
 
 #################
 # Parameters
 #################
-input_path = f"{root_path}/deep_upscale/Font-Awesome-SVG-PNG/black/svg"
+input_path = f"{root_path}/deep_upscale/images/raw/"
 output_path = f"{root_path}/deep_upscale/images/"
 
 poor_images_path = f"{output_path}poor/"
@@ -54,13 +49,60 @@ poor_width = 128
 poor_height = 128
 poor_dims = (poor_width, poor_height)
 
-x_small_factor_width  = 4
-x_small_factor_height  = 4
+x_small_factor_width = 3
+x_small_factor_height = 3
 
-rich_width = 512
-rich_height = 512
+rich_width = 128
+rich_height = 128
 
 samples_to_display = 10
+
+###############
+# Setup Inputs
+###############
+
+tmp_folder = f"{project_path}images/tmp/"
+if not os.path.exists(input_path):
+    os.makedirs(input_path)
+    
+if not os.path.exists(tmp_folder):
+    os.makedirs(tmp_folder)
+
+font_awesome_dir = f"{tmp_folder}font_awesome/black/svg/"
+bootstrap_dir = f"{tmp_folder}bootstrap-icons-1.1.0/"
+feather_dir = f"{tmp_folder}feather/icons/"
+
+###############
+# Get Data
+###############
+# Font Awesome
+os.system(f"git clone https://github.com/encharm/Font-Awesome-SVG-PNG.git {tmp_folder}font_awesome")
+
+# Bootstrap
+os.system(f"wget https://github.com/twbs/icons/releases/download/v1.1.0/bootstrap-icons-1.1.0.zip --directory-prefix={tmp_folder}")
+os.system(f"unzip {tmp_folder}*.zip -d {tmp_folder}")
+
+# Feather
+os.system(f"git clone https://github.com/feathericons/feather.git {tmp_folder}feather")
+
+
+
+#####################
+# Prevent Overwrites
+#####################
+os.chdir(font_awesome_dir)
+os.system("rename 's/^/fontawesome/' *")
+
+os.chdir(bootstrap_dir)
+os.system("rename 's/^/bootstrap/' *")
+
+os.chdir(feather_dir)
+os.system("rename 's/^/feather/' *")
+
+os.system(f"cp {font_awesome_dir}*.svg {input_path}")
+os.system(f"cp {bootstrap_dir}*.svg {input_path} ")
+os.system(f"cp {feather_dir}*.svg {input_path} ")
+
 
 ###################
 # Helper Functions
