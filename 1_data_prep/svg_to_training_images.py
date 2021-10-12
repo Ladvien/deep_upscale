8a6c457735e1e2c2a3799e4a482de5ab53426026
Casey Brittain <caseybrittain@Caseys-MBP.attlocal.net>
2021-10-12 14:57:35 -0500

    Reworking file renaming.

--- 1_data_prep/svg_to_training_images.py
+++ 1_data_prep/svg_to_training_images.py
@@ -125,153 +125,144 @@ if setup_data:
 # Prevent Overwrites
 #####################
 
-def prepend_category_name_to_files(category_name: str, directory: str):
+def prepend_category_name_to_files(category_name: str, directory: str, input_path):
     for count, filename in enumerate((os.listdir(directory))):
-        print(filename)
+        dest_path = f"{input_path}{category_name}_{filename}"
+        target_path = directory + filename 
+        os.system(f"mv {target_path} {dest_path}")
 
-prepend_category_name_to_files("test", font_awesome_dir)
+if setup_data:
 
-# if setup_data:
-#     os.chdir(font_awesome_dir)
-#     os.system("rename 's/^/fontawesome_/' *")
+    prepend_category_name_to_files("font_awesome", font_awesome_dir, input_path)
+    prepend_category_name_to_files("bootstrap_dir", bootstrap_dir, input_path)
+    prepend_category_name_to_files("feather_dir", feather_dir, input_path)
+    prepend_category_name_to_files("hero_solid_dir", hero_solid_dir, input_path)
+    prepend_category_name_to_files("hero_outline_dir", hero_outline_dir, input_path)
+    prepend_category_name_to_files("ionicons_dir", ionicons_dir, input_path)
+    prepend_category_name_to_files("bootstrap_dir", bootstrap_dir, input_path)
+
+    # os.system(f"cp {font_awesome_dir}*.svg {input_path}")
+    # os.system(f"cp {bootstrap_dir}*.svg {input_path} ")
+    # os.system(f"cp {feather_dir}*.svg {input_path} ")
+    # os.system(f"cp {hero_solid_dir}*.svg {input_path} ")
+    # os.system(f"cp {hero_outline_dir}*.svg {input_path} ")
+    # os.system(f"cp {ionicons_dir}*.svg {input_path}")
     
-#     os.chdir(bootstrap_dir)
-#     os.system("rename 's/^/bootstrap_/' *")
-    
-#     os.chdir(feather_dir)
-#     os.system("rename 's/^/feather_/' *")
-    
-#     os.chdir(hero_solid_dir)
-#     os.system("rename 's/^/solid_hero_/' *")    
-    
-#     os.chdir(hero_outline_dir)
-#     os.system("rename 's/^/outline_hero_/' *")
-    
-#     os.chdir(ionicons_dir)
-#     os.system("rename 's/^/ionicons_/' *")
-    
-#     os.system(f"cp {font_awesome_dir}*.svg {input_path}")
-#     os.system(f"cp {bootstrap_dir}*.svg {input_path} ")
-#     os.system(f"cp {feather_dir}*.svg {input_path} ")
-#     os.system(f"cp {hero_solid_dir}*.svg {input_path} ")
-#     os.system(f"cp {hero_outline_dir}*.svg {input_path} ")
-#     os.system(f"cp {ionicons_dir}*.svg {input_path}")
-    
-#     os.chdir(f"{project_path}")
+    # os.chdir(f"{project_path}")
 
 
-# ###################
-# # Helper Functions
-# ###################
-# def show_pil_image(image, title):
-#     plt.title(title)
-#     plt.imshow(np.array(image), cmap="gray")
-#     plt.axis("off")
-#     plt.show()    
+###################
+# Helper Functions
+###################
+def show_pil_image(image, title):
+    plt.title(title)
+    plt.imshow(np.array(image), cmap="gray")
+    plt.axis("off")
+    plt.show()    
 
-# def convert_pil2cv2(pil_image):
-#     return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+def convert_pil2cv2(pil_image):
+    return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-# def add_noise(image, num_pepper, shape_range, color_range, specks_per_pepper, group_range, size_range):
-#     noise_img = image.copy()
-#     for _ in range(num_pepper):
-#         # Radius of circle
-#         radius = randint(0, shape_range)
+def add_noise(image, num_pepper, shape_range, color_range, specks_per_pepper, group_range, size_range):
+    noise_img = image.copy()
+    for _ in range(num_pepper):
+        # Radius of circle
+        radius = randint(0, shape_range)
 
-#         b = randint(0, color_range)
-#         g = randint(0, color_range)
-#         r = randint(0, color_range)
+        b = randint(0, color_range)
+        g = randint(0, color_range)
+        r = randint(0, color_range)
 
-#         # BGR
-#         color = (b, g, r)
+        # BGR
+        color = (b, g, r)
 
-#         # Center coordinates
-#         y = randint(0, image.size[0])
-#         x = randint(0, image.size[1])
+        # Center coordinates
+        y = randint(0, image.size[0])
+        x = randint(0, image.size[1])
 
-#         for j in range(specks_per_pepper):        
+        for j in range(specks_per_pepper):        
 
-#             group_x_offset = randint(group_range*-1, group_range)
-#             group_y_offset = randint(group_range*-1, group_range)
+            group_x_offset = randint(group_range*-1, group_range)
+            group_y_offset = randint(group_range*-1, group_range)
 
-#             # Size
-#             radius = randint(0, size_range)
+            # Size
+            radius = randint(0, size_range)
 
-#             # Convert from PIL to cv2.
-#             noise_img = convert_pil2cv2(noise_img)
+            # Convert from PIL to cv2.
+            noise_img = convert_pil2cv2(noise_img)
 
-#             # Add noise.
-#             noise_img = cv2.circle(noise_img, (x + group_x_offset, y + group_y_offset), radius, color, -1)
+            # Add noise.
+            noise_img = cv2.circle(noise_img, (x + group_x_offset, y + group_y_offset), radius, color, -1)
 
-#     # Return PIL image.
-#     return Image.fromarray(noise_img)
+    # Return PIL image.
+    return Image.fromarray(noise_img)
 
-# ###################
-# # Make directories
-# ###################
-# def make_dir(path):
-#     if not os.path.exists(path):
-#         os.makedirs(path)
-
-
-# make_dir(poor_images_path)
-# make_dir(rich_images_path)
-
-# #################
-# # Get File Paths
-# #################
-# os.chdir(input_path)
-# svg_file_paths = [input_path + "/" + file for file in glob.glob("*.svg")]
-
-# ###################
-# # Generate Images
-# ###################
-
-# index = 0
-# for file_path in svg_file_paths:
-
-#     # Get image file name.
-#     file_name = file_path.split("/")[-1].replace(".svg", ".png")
-
-#     # Paths
-#     poor_file_path = poor_images_path + file_name
-#     rich_file_path = rich_images_path + file_name
-
-#     # Save really small.    
-#     cairosvg.svg2png(
-#         url=file_path,
-#         write_to=poor_file_path,
-#         output_width=round(poor_width / x_small_factor_width),
-#         output_height=round(poor_width / x_small_factor_height),
-#         background_color="ghostwhite",
-#     )
-
-#     # Load the really small
-#     xs_image = Image.open(poor_file_path)
-#     # Make it target size for poor image.
-#     poor_image = xs_image.resize(((poor_dims)))
-#     poor_image = add_noise(poor_image, num_pepper, shape_range, color_range, specks_per_pepper, group_range, size_range)
+###################
+# Make directories
+###################
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
-#     # Save it again.
-#     poor_image.save(poor_file_path)
+make_dir(poor_images_path)
+make_dir(rich_images_path)
+
+#################
+# Get File Paths
+#################
+os.chdir(input_path)
+svg_file_paths = [input_path + "/" + file for file in glob.glob("*.svg")]
+
+###################
+# Generate Images
+###################
+
+index = 0
+for file_path in svg_file_paths:
+
+    # Get image file name.
+    file_name = file_path.split("/")[-1].replace(".svg", ".png")
+
+    # Paths
+    poor_file_path = poor_images_path + file_name
+    rich_file_path = rich_images_path + file_name
+
+    # Save really small.    
+    cairosvg.svg2png(
+        url=file_path,
+        write_to=poor_file_path,
+        output_width=round(poor_width / x_small_factor_width),
+        output_height=round(poor_width / x_small_factor_height),
+        background_color="ghostwhite",
+    )
+
+    # Load the really small
+    xs_image = Image.open(poor_file_path)
+    # Make it target size for poor image.
+    poor_image = xs_image.resize(((poor_dims)))
+    poor_image = add_noise(poor_image, num_pepper, shape_range, color_range, specks_per_pepper, group_range, size_range)
 
 
-#     # Save really rich.    
-#     cairosvg.svg2png(
-#         url=file_path,
-#         write_to=rich_file_path,
-#         output_width=rich_width,
-#         output_height=rich_height,
-#         background_color="ghostwhite",
-#     )
-#     rich_image = Image.open(rich_file_path)
+    # Save it again.
+    poor_image.save(poor_file_path)
 
-#     index += 1
 
-#     # Show what what's going on.
-#     if index < samples_to_display:
-#         show_pil_image(poor_image, "Poor")
-#         show_pil_image(rich_image, "Rich")
-#     elif just_sample:
-#         break
+    # Save really rich.    
+    cairosvg.svg2png(
+        url=file_path,
+        write_to=rich_file_path,
+        output_width=rich_width,
+        output_height=rich_height,
+        background_color="ghostwhite",
+    )
+    rich_image = Image.open(rich_file_path)
+
+    index += 1
+
+    # Show what what's going on.
+    if index < samples_to_display:
+        show_pil_image(poor_image, "Poor")
+        show_pil_image(rich_image, "Rich")
+    elif just_sample:
+        break
